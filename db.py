131b1327fae72fb6176bbb5943ef71cbf4950ce8
Joshua Tom <wufflesinblue@gmail.com>
2023-12-03 22:09:52 -0700

    Moved Daniels code around because code broken

--- db.py
+++ db.py
@@ -1,36 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask
 
-# For more information about database configuration checkout out the docs
-# https://flask-sqlalchemy.palletsprojects.com/en/3.0.x/
 db = SQLAlchemy()
 db_name = "database.db"
-
-# there is no hope
-
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_name}'
     db.init_app(app)
+    
     with app.app_context():
+        # Define models within the app context
+        class WordDifficulty(db.Model):
+            __tablename__ = "difficulty"
+            word_id = db.Column(db.Integer, primary_key=True)
+            hard = db.Column(db.String(50))
+            medium = db.Column(db.String(50))
+            easy = db.Column(db.String(50))
+
+        class Time(db.Model):
+            __tablename__ = "clock"
+            word2_id = db.Column(db.Integer, primary_key=True)
+            ran_out = db.Column(db.String(50))
+            on_time = db.Column(db.String(50))
+
+        # Create the tables
         db.create_all()
+    
     return app
-
-class word_diffuclty(db.Model):
-    __tablename__ = "difficulty"
-    word_id = db.Column(db.Integer, primary_key=True)
-    hard = db.Column(db.String)
-    medium = db.Column(db.String)
-    easy = db.Column(db.String)
-
-class time(db.Model):
-    __tablename__ = "clock"
-    word2_id = db.Column(db.Integer, primary_key=True)
-    ran_out=db.Column(db.String)
-    on_time = db.Column(db.string)
-
-db.create_all()
 
 #I HAVE SETUP THE DATABASE I REPEAT THERE IS A DATABASE... i just need to figure out how to update the database with every typing test logged.
 
