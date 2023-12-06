9f7221edc0e85926100238aefefbc708895191c3
Your Name <you@example.com>
2023-12-06 16:56:42 -0600

    wonderwords word amount changes

--- words.py
+++ words.py
@@ -11,12 +11,12 @@ def create_sentence(length):
 
 def easy_words():
     w = RandomWord()
-    text = [w.word(word_max_length=4) for _ in range(2)]
+    text = [w.word(word_max_length=4) for _ in range(30)]
     return text
 
 def medium_words():
     text = []
-    for i in range(0,20):
+    for i in range(0,25):
         s = RandomSentence()
         w = RandomWord()
         text.append(w.word(word_min_length=7, word_max_length=11))
@@ -25,7 +25,7 @@ def medium_words():
 
 def hard_words():
     text = []
-    for i in range(0,100):
+    for i in range(0,20):
         s = RandomSentence()
         w = RandomWord()
         text.append(w.word(word_min_length=14))
